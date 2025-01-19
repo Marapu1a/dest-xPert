d1b255e846d89abb6ed1951cc776b6ff718cea81
Valentin <marapulets87@yandex.ru>
2025-01-19 20:04:02 +0300

    CI/CD

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,13 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // Это алиас для папки src
-      '@assets': path.resolve(__dirname, './src/assets'), // Алиас для изображений
-      '@pages': path.resolve(__dirname, './src/pages'), // Алиас для pages
-      '@components': path.resolve(__dirname, './src/components'), // Алиас для components
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@components': path.resolve(__dirname, './src/components'),
     },
+  },
+  build: {
+    outDir: '/var/www/destxpert.travel', // Путь на сервере
   },
 });
