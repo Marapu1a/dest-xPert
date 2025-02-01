f9e8b5ec4f43d8624979bf3d62c01eec05df6006
Valentin <marapulets87@yandex.ru>
2025-02-02 01:42:33 +0300

    смена слайдов и банеров 02.02.2025

--- src/pages/home/header/Carousel.jsx
+++ src/pages/home/header/Carousel.jsx
@@ -5,79 +5,50 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-import image1 from '@assets/header/danang.webp';
-import image2 from '@assets/header/kambodja.webp';
-import image3 from '@assets/header/fukuok.webp';
-import image4 from '@assets/header/niachang.webp';
-import image5 from '@assets/header/prague.webp';
-import image6 from '@assets/header/posh-club.webp';
-import image7 from '@assets/header/hajnan.webp';
-import image8 from '@assets/header/tajland.webp';
-import image9 from '@assets/header/tg-bot.webp';
-import image10 from '@assets/header/waterworld.webp';
+import image1 from '@assets/header/Eurotour.webp';
+import image2 from '@assets/header/Roadshow.webp';
+import image3 from '@assets/header/Popular.webp';
+import image4 from '@assets/header/TG.webp';
+import image5 from '@assets/header/Training.webp';
 
 const slides = [
   {
     image: image1,
-    title: 'Дананг - сердце Вьетнама',
-    subtitle: 'Прямые вылеты из Астаны и Алматы!',
-    buttonText: 'Бронируйте туры',
+    title: '',
+    subtitle: '',
+    buttonText: 'Бронировать',
+    buttonStyle:
+      'absolute bottom-7 md:bottom-[90px] left-2 md:left-[54px] bg-orange-500 text-white text-[28px] md:text-[48px] md:pb-4 pb-2 font-bold py-1 px-10 rounded-full shadow-lg hover:bg-orange-600 transition',
   },
   {
     image: image2,
-    title: 'Экзотика Камбоджи',
-    subtitle: 'Прямой чартер из Алматы, Астаны и Екатеринбурга!',
-    buttonText: 'Бронируйте туры',
+    title: '',
+    subtitle: '',
+    buttonText: 'Регистрация',
+    buttonStyle:
+      'absolute bottom-7 md:bottom-[90px] left-2 md:left-[48px] bg-orange-500 text-white text-[28px] md:text-[48px] md:pb-4 pb-2 font-bold py-1 px-10 rounded-full shadow-lg hover:bg-orange-600 transition',
   },
   {
     image: image3,
-    title: 'Вьетнам, остров Фукуок',
-    subtitle: 'о. Фукуок из Алматы, Астаны и Шымкента доступен к бронированию!',
-    buttonText: 'Бронируйте туры',
+    title: '',
+    subtitle: '',
+    buttonText: '',
+    buttonStyle: 'hidden',
   },
   {
     image: image4,
-    title: 'Нячанг из Алматы!',
-    subtitle: 'Открывая новые горизонты! Нячанг из Алматы 2 раза в неделю',
-    buttonText: 'Бронируйте туры',
+    title: '',
+    subtitle: '',
+    buttonText: 'Перейти',
+    buttonStyle:
+      'absolute bottom-7 md:bottom-[180px] left-2 md:left-[105px] bg-blue-500 text-white text-[38px] font-bold md:pb-5 py-3 px-14 rounded-full shadow-lg hover:bg-blue-600 transition',
   },
   {
     image: image5,
-    title: 'Прага из Астаны!',
-    subtitle: 'Вылеты из Астаны (с подлетами на крыльях Scat Airlines)',
-    buttonText: 'Бронируйте туры',
-  },
-  {
-    image: image6,
-    title: 'Posh Club by Sunrise',
-    subtitle: 'Бонусная система для сьют-номеров в отелях Sunrise',
-    buttonText: 'Забронировать отель',
-  },
-  {
-    image: image7,
-    title: 'Китай. Остров Хайнань',
-    subtitle:
-      'Китайский остров сокровищ. Вылеты 2 раза в неделю из Алматы, Астаны.',
-    buttonText: 'Бронируйте туры',
-  },
-  {
-    image: image8,
-    title: 'Тайланд круглый год',
-    subtitle:
-      'Вылеты из Актобе, Алматы, Астаны, Костаная, Тараза, Уральска и Шымкента',
-    buttonText: 'Бронируйте туры',
-  },
-  {
-    image: image9,
-    title: 'Telegram-бот',
-    subtitle: 'Описание функций бота',
-    buttonText: 'Перейти в Telegram',
-  },
-  {
-    image: image10,
-    title: 'Coral Sea Waterworld',
-    subtitle: 'Ваш отель для семейного отдыха',
-    buttonText: 'Бронируйте туры',
+    title: '',
+    subtitle: '',
+    buttonText: '',
+    buttonStyle: 'hidden',
   },
 ];
 
@@ -87,7 +58,7 @@ const Carousel = () => (
       modules={[Navigation, Pagination, Autoplay]}
       navigation
       pagination={{ clickable: true }}
-      autoplay={{ delay: 3000, disableOnInteraction: false }}
+      // autoplay={{ delay: 3000, disableOnInteraction: false }}
       loop
     >
       {slides.map((slide, index) => (
@@ -96,17 +67,16 @@ const Carousel = () => (
             src={slide.image}
             alt={slide.title}
             className="w-full h-[300px] sm:h-[400px] md:h-[500px] object-cover"
+            loading="lazy"
           />
-          <div className="absolute inset-0  flex flex-col justify-center items-center text-white">
+          <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
             <h2 className="text-2xl md:text-4xl font-bold cursor-default">
               {slide.title}
             </h2>
             <p className="text-lg md:text-xl mt-2 cursor-default">
               {slide.subtitle}
             </p>
-            <button className="mt-4 bg-[#f5f5dc] text-blue-600 px-4 py-2 rounded-lg hover:bg-blue-400 hover:text-white transition-all">
-              {slide.buttonText}
-            </button>
+            <button className={slide.buttonStyle}>{slide.buttonText}</button>
           </div>
         </SwiperSlide>
       ))}
--- src/pages/home/sections/Attention.jsx
+++ src/pages/home/sections/Attention.jsx
@@ -6,71 +6,61 @@ import attention2 from '@assets/sections/attention-2.webp';
 import attention3 from '@assets/sections/attention-3.webp';
 import attention4 from '@assets/sections/attention-4.webp';
 
+const slides = [
+  {
+    img: attention1,
+    title: '',
+    text: '',
+    buttonStyle:
+      'absolute md:left-[28px] md:top-[250px] z-10 text-center max-w-[80%]',
+  },
+  {
+    img: attention2,
+    title: '',
+    text: '',
+    buttonStyle:
+      'absolute md:left-[38px] md:top-[190px] z-10 text-center max-w-[80%]',
+  },
+  {
+    img: attention3,
+    title: '',
+    text: '',
+    buttonStyle:
+      'absolute md:left-[52px] md:top-[250px] z-10 text-center max-w-[80%]',
+  },
+  {
+    img: attention4,
+    title: '',
+    text: '',
+    buttonStyle:
+      'absolute md:left-[32px] md:top-[210px] z-10 text-center max-w-[80%]',
+  },
+];
+
 const AttentionSection = () => {
   return (
     <div className="bg-white p-6">
       <h2 className="text-3xl font-bold mb-6">Обратите внимание</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {[
-          {
-            img: attention1,
-            title: 'Селфи Премиум',
-            text: 'Выбирая тур с отметкой «Selfie Premium», автоматически будут предложены отели класса люкс, встреча с цветами, индивидуальный трансфер.',
-            bgColor: 'bg-blue-400',
-          },
-          {
-            img: attention2,
-            title: 'Чат-бот MAX',
-            text: 'Max: телеграм-бот и персональный помощник. Удобно для смартфона. Доступ 24/7. Оперативный ответ на запрос.',
-            bgColor: 'bg-blue-300',
-          },
-          {
-            img: attention3,
-            title: 'Обучение',
-            text: 'Регулярно проводятся образовательные мероприятия для турагентов в разнообразных форматах.',
-            bgColor: 'bg-blue-200',
-          },
-          {
-            img: attention4,
-            title: 'Коллаборация',
-            text: 'Открыты к сотрудничеству и партнерству. Работая в сфере услуг, мы понимаем, как много зависит от хороших коллабораций.',
-            bgColor: 'bg-blue-500',
-          },
-        ].map(({ img, title, text, bgColor }, index) => (
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        {slides.map(({ img, title, text, buttonStyle }, index) => (
           <div
             key={index}
-            className={`attention-block ${bgColor} text-white p-6 rounded-lg flex flex-col md:flex-row overflow-hidden`}
+            className="relative flex items-center justify-center text-white p-6 rounded-lg overflow-hidden min-h-[250px] md:min-h-[700px]"
+            style={{
+              backgroundImage: `url(${img})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
           >
-            <div className="w-full md:w-1/2 flex flex-col justify-center mb-4 md:mb-0">
-              <h3 className="text-xl font-semibold text-shadow-md mb-2">
-                {title}
-              </h3>
-              <p className="text-shadow-md mb-4">{text}</p>
+            <div className={buttonStyle}>
+              <h3 className="hidden text-xl font-semibold mb-2">{title}</h3>
+              <p className="hidden mb-4">{text}</p>
               <Link
                 to="#"
-                className={`inline-block font-bold text-center px-6 py-3 rounded-full border-2 border-blue-300 shadow-xl transition-transform transform hover:scale-105 hover:bg-opacity-80`}
-                style={{
-                  maxWidth: '200px',
-                  backgroundColor: 'white',
-                  color:
-                    bgColor.split('-')[1] === 'blue'
-                      ? '#1E3A8A'
-                      : bgColor.split('-')[1] === 'green'
-                        ? '#1E40AF'
-                        : bgColor.split('-')[1] === 'yellow'
-                          ? '#3B82F6'
-                          : '#2563EB',
-                }}
+                className="inline-block font-bold text-center px-6 py-3 rounded-full bg-white text-blue-900 md:text-[28px] shadow-xl transition-transform transform hover:scale-105"
               >
                 Подробнее
               </Link>
-            </div>
-            <div className="w-full md:w-1/2 flex items-stretch">
-              <img
-                src={img}
-                alt={title}
-                className="w-full h-full object-cover rounded-lg"
-              />
             </div>
           </div>
         ))}
